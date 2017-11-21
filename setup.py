0c778ef69d8050417b6f6013b8e150df58fe689b
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-11-21 17:50:13 +0000

    1st kinda-working version for Robotarium

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='qsubmit',
+    version='0.0.1dev',
+    description='Batch engine job submission wrapper',
+    author='Ondrej Dusek',
+    author_email='o.dusek@hw.ac.uk',
+    url='https://github.com/tuetschek/qsubmit',
+    download_url='https://github.com/tuetschek/qsubmit.git',
+    license='Apache 2.0',
+    scripts=['bin/qsubmit'],
+)
+
