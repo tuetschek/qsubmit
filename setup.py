58b5d69605a4a0a373171f52bf8661ccef8dbc4e
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2021-09-22 13:07:59 +0200

    Bugfix

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='qsubmit',
-    version='0.0.3dev',
+    version='0.0.2dev',
     description='Batch engine job submission wrapper',
     author='Ondrej Dusek',
-    author_email='odusek@ufal.mff.cuni.cz',
+    author_email='o.dusek@hw.ac.uk',
     url='https://github.com/tuetschek/qsubmit',
     download_url='https://github.com/tuetschek/qsubmit.git',
     license='Apache 2.0',
